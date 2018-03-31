aea58f4cb5adbabd14c60b4595b457f7268cf866
Swapnil Khandekar <swa.khandekar@gmail.com>
2018-03-31 21:53:40 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    publicPath: "/",
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/},
+      {test: /\.js$/, loader: "source-map-loader", enforce: "pre"}
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html', inject: 'body'}),
+  ],
+
+  mode: "development"
+};
