615eac377623f36df393e4a78dc17bf9e6f9b00c
Swapnil Khandekar <swa.khandekar@gmail.com>
2018-04-07 20:15:06 +0530

    Add thunk as middleware

--- src/reduxHandles.js
+++ src/reduxHandles.js
@@ -1,9 +1,13 @@
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {combineReducers} from "redux";
 import {matrixReducer} from "./components/Matrix/reducer";
+import thunk from "redux-thunk";
 
 const combinedReducers = combineReducers({
   matrix: matrixReducer
 });
 
-export const store = createStore(combinedReducers);
+export const store = createStore(
+  combinedReducers,
+  applyMiddleware(thunk)
+);
